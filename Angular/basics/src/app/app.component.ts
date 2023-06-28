11b68a3ecd97ddc682c97d9ee1d761cdc8c75f16
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-06-28 08:47:34 +0300

    up

--- Angular/basics/src/app/app.component.ts
+++ Angular/basics/src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Hello,world</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
+  // styles: [''],
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Nikolay';
+  imgURL = 'https://picsum.photos/id/237/500/500';
+
+  getName(): string {
+    return this.name;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
 }
