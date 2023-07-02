784d2abbdbbfddd25ed19c5e188b7cc222973c4a
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-07-02 15:08:44 +0300

    up

--- Angular/clips/src/app/services/modal.service.ts
+++ Angular/clips/src/app/services/modal.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+interface IModal {
+  id: string;
+  visible: boolean;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
+  private modals: IModal[] = [];
+  constructor() {}
 
-  constructor() { }
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false,
+    });
+  }
+
+  isModalOpen(id: string): boolean {
+    return !!this.modals.find((x) => x.id === id)?.visible;
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find((x) => x.id === id);
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
+  }
 }
--- Angular/clips/src/app/shared/modal/modal.component.ts
+++ Angular/clips/src/app/shared/modal/modal.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  @Input() modalId = '';
 
+  constructor(public modal: ModalService) {}
+
+  ngOnInit(): void {}
+
+  closeModal() {
+    this.modal.toggleModal(this.modalId);
+  }
 }
