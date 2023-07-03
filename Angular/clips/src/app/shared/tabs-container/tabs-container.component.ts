4543c65ddaae1830cd128f9931b00f2b1cf209a4
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-07-03 08:23:27 +0300

    up

---
+++ Angular/clips/src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  AfterContentInit,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css'],
+})
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
+
+  constructor() {}
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((x) => x.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((x) => {
+      x.active = false;
+    });
+    tab.active = true;
+
+    return false;
+  }
+}
