ed1fd4a198c709ca61e742870e631fab1358b310
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-08-23 20:24:06 +0300

    up

--- Angular/clips/src/app/clips-list/clips-list.component.ts
+++ Angular/clips/src/app/clips-list/clips-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { ClipService } from '../services/clip.service';
 import { IClip } from '../models/clip.model';
 import { DatePipe } from '@angular/common';
@@ -10,15 +10,23 @@ import { DatePipe } from '@angular/common';
   providers: [DatePipe],
 })
 export class ClipsListComponent implements OnInit, OnDestroy {
+  @Input() scrollable = true;
+
   constructor(public clipService: ClipService) {
     this.clipService.getClips();
   }
 
   ngOnInit(): void {
-    window.addEventListener('scroll', this.handleScroll);
+    if (this.scrollable) {
+      window.addEventListener('scroll', this.handleScroll);
+    }
   }
   ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.handleScroll);
+    if (this.scrollable) {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    this.clipService.pageClips = [];
   }
 
   handleScroll = () => {
