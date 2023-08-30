eacc3c29d725a55c84f5c78bc6e993cc8797f656
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-08-30 10:16:56 +0300

    up

--- ReactJS/advices/src/App.js
+++ ReactJS/advices/src/App.js
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [advice, newAdvice] = useState("Get React Advice");
+  const [advice, newAdvice] = useState("");
   const [count, newCount] = useState(0);
 
   async function getAvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    console.log(data.slip.advice);
     newAdvice(data.slip.advice);
     newCount((c) => c + 1);
   }
+
+  useEffect(function () {
+    getAvice();
+  }, []);
 
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAvice}>Get advice</button>
-      <p>
-        You have read <strong>{count}</strong> pieces of advice
-      </p>
+      <Message count={count} />
     </div>
+  );
+}
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice
+    </p>
   );
 }
 
