0eda632869f2c9d35af2cbd181a8be3f73f9b4ff
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-08-30 23:14:05 +0300

    up

--- ReactJS/03.pizza-menu/src/index.js
+++ ReactJS/03.pizza-menu/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { v4 as uuid } from "uuid";
+
+import { pizzaData } from "./data.js";
 
 // import Pizza from "./components/Pizza.js";
 
@@ -30,47 +33,65 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // my way
+  const numPizzas = !!pizzas.length;
+  // Jonas way
+  // const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        img="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushrooms"
-        img="pizzas/funghi.jpg"
-        price={12}
-      />
+      {/* my way */}
+      {numPizzas && (
+        // Jonas way
+        // {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            // my way
+            <Pizza pizzaObj={pizza} key={uuid()} />
+            // Jonas way
+            // <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
 
 function Pizza(props) {
+  console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.img} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
 function Footer() {
   const hour = new Date().getHours();
-  const currentTime = new Date().toLocaleTimeString();
-  const isOpen = hour > 7 && hour < 19;
+  const minutes = new Date().getMinutes();
+  const openHour = 12;
+  const closeHour = 23;
+  // const currentTime = new Date().toLocaleTimeString();
+  const currentTime = `${hour}:${minutes}`;
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="footer">
-      {isOpen
-        ? `${currentTime} - We\'re currently open!`
-        : `${currentTime} - We\'re closed!`}
+      {/* {isOpen
+        ? `${currentTime} - We're currently open!`
+        : `${currentTime} - We're closed!`} */}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
