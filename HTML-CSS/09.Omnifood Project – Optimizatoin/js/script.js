4f0a83de2dee49dffbaf8c072964dea1e544a536
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-10-17 23:44:20 +0300

    up

--- HTML-CSS/09.Omnifood Project – Optimizatoin/js/script.js
+++ HTML-CSS/09.Omnifood Project – Optimizatoin/js/script.js
@@ -42,6 +42,30 @@ allLinks.forEach((link) =>
 );
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px", //this is the height of the nav container
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
