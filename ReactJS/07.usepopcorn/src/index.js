19b252355e7eebe5e1cc27fe1b52fb6da67fc771
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-10-21 14:53:15 +0300

    up

--- ReactJS/07.usepopcorn/src/index.js
+++ ReactJS/07.usepopcorn/src/index.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarsRating from "./StarRating.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function ExternalStars() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarsRating color="#FFA81E" maxRating={4} onSetRating={setMovieRating} />
+      <p>This movie was {movieRating} stars</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarsRating maxRating={5} />
+    <StarsRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarsRating size={32} className={"test"} defaultRating={3} color="red" />
+
+    <ExternalStars />
   </React.StrictMode>
 );
