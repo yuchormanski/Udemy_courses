822f5cf4814f7faee4089174edcd658cf89fb439
yuchormanski <yuchormanski@gmail.com>
2024-01-21 23:56:50 +0200

    up

--- ReactJS/14.fast-react-pizza/src/features/order/SearchOrder.jsx
+++ ReactJS/14.fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -18,6 +18,7 @@ function SearchOrder() {
         placeholder={"Search order #"}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="duration:300 w-28 rounded-full bg-yellow-100 px-4 py-2 transition-all placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:focus:w-72 md:w-64"
       />
     </form>
   );
