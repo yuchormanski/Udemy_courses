d71502e95d9ad8181a739f6ea9738cae8362120e
yuchormanski <yuchormanski@gmail.com>
2024-01-21 22:42:37 +0200

    up

--- ReactJS/14.fast-react-pizza/src/ui/Header.jsx
+++ ReactJS/14.fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/Username.jsx";
 
 function Header() {
   return (
-    <header>
-      <Link to={"/"}>Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to={"/"} className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
--- ReactJS/14.fast-react-pizza/src/ui/Home.jsx
+++ ReactJS/14.fast-react-pizza/src/ui/Home.jsx
@@ -1,11 +1,16 @@
+import CreateUser from "../features/user/CreateUser.jsx";
+
 function Home() {
   return (
-    <div>
-      <h1>
+    <div className="my-10 text-center sm:my-16">
+      <h1 className="mb-4 text-xl font-semibold">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+      <CreateUser />
     </div>
   );
 }
