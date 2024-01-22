457041c1bf61caf6c5aba2d08549915ef0e9d165
yuchormanski <yuchormanski@gmail.com>
2024-01-22 08:43:46 +0200

    up

--- ReactJS/14.fast-react-pizza/src/features/order/CreateOrder.jsx
+++ ReactJS/14.fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,8 +1,9 @@
 // import { useState } from "react";
 
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant.js";
-import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant.js';
+import { useState } from 'react';
+import Button from '../../ui/Button.jsx';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -13,21 +14,21 @@ const isValidPhone = (str) =>
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -36,7 +37,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
   // const [phoneInput, setPhoneInput] = useState("");
 
@@ -96,14 +97,15 @@ function CreateOrder() {
 
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
-          <button
+          {/* <button
             disabled={isSubmitting}
             className="inline-block 
             rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase
             tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
-            {isSubmitting ? "Placing order ..." : "Order now"}
-          </button>
+          ></button> */}
+          <Button disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order ...' : 'Order now'}
+          </Button>
         </div>
       </Form>
     </div>
@@ -117,13 +119,13 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   const errors = {};
   if (!isValidPhone(order.phone)) {
     errors.phone =
-      "Please, give us your correct phone number. We might need it to contact you";
+      'Please, give us your correct phone number. We might need it to contact you';
   }
   if (Object.keys(errors).length > 0) return errors;
 
--- ReactJS/14.fast-react-pizza/src/ui/AppLayout.jsx
+++ ReactJS/14.fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,11 +1,11 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview.jsx";
-import Header from "./Header.jsx";
-import Loader from "./Loader.jsx";
+import { Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview.jsx';
+import Header from './Header.jsx';
+import Loader from './Loader.jsx';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
