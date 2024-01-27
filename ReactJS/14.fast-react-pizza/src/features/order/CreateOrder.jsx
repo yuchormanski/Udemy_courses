d3fa0506b4956c574e3170ff0458c69cc030f53e
yuchormanski <yuchormanski@gmail.com>
2024-01-27 12:56:26 +0200

    up

--- ReactJS/14.fast-react-pizza/src/features/order/CreateOrder.jsx
+++ ReactJS/14.fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -4,12 +4,13 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant.js';
 import { useState } from 'react';
 import Button from '../../ui/Button.jsx';
-import { useSelector } from 'react-redux';
-import { getUsername } from '../user/userSlice.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAddress, getUsername } from '../user/userSlice.js';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice.js';
 import EmptyCart from '../cart/EmptyCart.jsx';
 import store from '../../store.js';
 import { formatCurrency } from '../../utils/helpers.js';
+import { getAddress } from '../../services/apiGeocoding.js';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -19,7 +20,13 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector(getUsername);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
   const cart = useSelector(getCart);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
@@ -27,8 +34,11 @@ function CreateOrder() {
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
+  const dispatch = useDispatch();
+  const isLoadingAddress = addressStatus === 'loading';
 
   if (!cart.length) return <EmptyCart />;
+
   return (
     <div className={'px-4 py-6'}>
       <h2 className={'mb-8 text-xl font-semibold'}>
@@ -63,16 +73,45 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className={'mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'}>
+        <div
+          className={
+            'relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'
+          }
+        >
           <label className={'sm:basis-40'}>Address</label>
           <div className={'grow'}>
             <input
               className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {addressStatus === 'error' && (
+              <p
+                className={
+                  'text - xs mt-2 rounded-md bg-red-100 p-2 text-red-700'
+                }
+              >
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className={'absolute right-[3px] z-50'}>
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className={'mb-12 flex items-center gap-5'}>
@@ -90,6 +129,15 @@ function CreateOrder() {
         </div>
 
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+        <input
+          type="hidden"
+          name="position"
+          value={
+            position.longitude && position.latitude
+              ? `${position.latitude},${position.longitude}`
+              : ''
+          }
+        />
         <div>
           {/* <button
             disabled={isSubmitting}
@@ -97,7 +145,7 @@ function CreateOrder() {
             rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase
             tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
           ></button> */}
-          <Button disabled={isSubmitting} type="primary">
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting
               ? 'Placing order ...'
               : `Order now for ${formatCurrency(totalPrice)}`}
@@ -118,6 +166,7 @@ export async function action({ request }) {
     priority: data.priority === 'true',
   };
 
+  console.log(order);
   const errors = {};
   if (!isValidPhone(order.phone)) {
     errors.phone =
