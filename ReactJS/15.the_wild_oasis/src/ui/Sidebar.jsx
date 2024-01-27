c85d3911a3f187af3e3368c1f7afabee5f47874c
yuchormanski <yuchormanski@gmail.com>
2024-01-28 01:22:50 +0200

    up

--- ReactJS/15.the_wild_oasis/src/ui/Sidebar.jsx
+++ ReactJS/15.the_wild_oasis/src/ui/Sidebar.jsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
+import Logo from "./Logo.jsx";
+import MainNav from "./MainNav.jsx";
+import { memo } from "react";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2%.4;
   border-right: 0.1rem solid var(--color-grey-100);
   grid-row: 1/-1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <StyledSidebar>ASIDE</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
-export default Sidebar;
+export default memo(Sidebar);
